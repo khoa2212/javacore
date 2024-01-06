a5708bd0aee4762d7ca423ef0ae5092950692439
Nguyen Tuan Khoa <nguyenkhoa438@gmail.com>
2024-01-06 21:18:24 +0700

    Practice Deck of cards

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,21 @@ import java.util.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Collection<String> list = new TreeSet<>();
-
-        String[] names = {"Anna", "Bob", "Carol", "David", "Edna"};
-        list.addAll(Arrays.asList(names));
-        System.out.println(list);
-
-        list.add("Fred");
-        list.addAll(Arrays.asList("George", "Gary", "Grace"));
-        System.out.println(list);
-        System.out.println("Gary is in the list? " + list.contains("Gary"));
-
-        list.removeIf(s -> s.charAt(0) == 'G');
-        System.out.println(list);
-        System.out.println("Gary is in the list? " + list.contains("Gary"));
+//        Collection<String> list = new TreeSet<>();
+//
+//        String[] names = {"Anna", "Bob", "Carol", "David", "Edna"};
+//        list.addAll(Arrays.asList(names));
+//        System.out.println(list);
+//
+//        list.add("Fred");
+//        list.addAll(Arrays.asList("George", "Gary", "Grace"));
+//        System.out.println(list);
+//        System.out.println("Gary is in the list? " + list.contains("Gary"));
+//
+//        list.removeIf(s -> s.charAt(0) == 'G');
+//        System.out.println(list);
+//        System.out.println("Gary is in the list? " + list.contains("Gary"));
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
     }
 }
