d10306965e51edc72eb498013cb07710c52f67b2
Nguyen Tuan Khoa <nguyenkhoa438@gmail.com>
2024-01-07 11:27:52 +0700

    continue practice lamda

--- src/Main.java
+++ src/Main.java
@@ -52,5 +52,17 @@ public class Main {
         people.sort(comparatorMixed);
         System.out.println(people);
 
+        int result = calculator((var a, var b) -> a + b,5, 2);
+        var result2 = calculator((a, b) -> a / b, 10.0, 2.5);
+        var result3 = calculator(
+                (a, b) -> a.toUpperCase() + " " + b.toUpperCase(),
+                "Ralph", "Kramden");
+    }
+
+    public static <T> T calculator(Operation<T> function, T value1, T value2) {
+
+        T result = function.operate(value1, value2);
+        System.out.println("Result of operation: " + result);
+        return result;
     }
 }
